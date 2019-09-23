bbcfdadcfaaa5a5a573ff22da80494ecb05f22b1
José Jaime Méndez Flores <you@example.com>
2019-09-23 19:48:21 +0200

    QR Factorization completed. Pending calculate beta-hat and variance of beta-hat

--- lab4/R/Lab4.R
+++ lab4/R/Lab4.R
@@ -1,7 +1,25 @@
 # 1.2.2 (*) Using the QR decomposition
 linreg <- function(formula, data){
   X <- model.matrix(formula, data)
+  X <- unname(X)
+  X <- matrix(c(12,  93,  35, 102,  62,  -8,  67,  24,  45,   4,  99,  16), ncol = 3, byrow = TRUE)
   dvar <- all.vars(formula)[1]
   m <- dim(X)[1]
   n <- dim(X)[2]
+  Q <- diag(m)
+  for (k in 1:n){
+    #Find the HH reflector
+    z <- matrix(X[k:m,k])
+    v <- c(-sign(z[1]) * sqrt(sum(z^2)) - z[1], -z[2:length(z)])
+    v <- v/c(sqrt(t(v) %*% v))
+    v <- matrix(v, ncol=1)
+    for (j in 1:n){
+      X[k:m,j] <- X[k:m,j]-(v*c(2*(t(v)%*%X[k:m, j])))
+    }
+    for (i in 1:m){
+      Q[k:m,j] <- Q[k:m,j]-(v*c(2*(t(v)%*%Q[k:m, j])))
+    }
+  }
+  Q <- t(Q)
+  R <- X*upper.tri(X, diag=T)
 }
